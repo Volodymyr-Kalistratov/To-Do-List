53bb4eb8812c6eb486f55fae65217a6f4a7f46c0
Volodymyr Kalistratov <volodymyr.kalistratov@gmail.com>
2023-04-08 18:56:30 +0200

    remove Tasks

--- script.js
+++ script.js
@@ -10,6 +10,19 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+    
     const render = () => {
         let htmlString = "";
 
@@ -18,21 +31,22 @@
         <li
         ${task.done ? " style=\"text-decoration: line-through\"" : ""
                 }>
+                <button class="js-remove">usuń</button>
             ${task.content}
         </li >
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+            removeTask(index);
+            });
         });
-
-        render();
-    }; 
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -45,7 +59,7 @@
 
         addNewTask(newTaskContent);
     };
-    
+
     const init = () => {
         render();
 
